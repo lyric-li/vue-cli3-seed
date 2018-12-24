fdf1349e0375298f8e513e19c7118f42abf3a243
lyric <1353758518@qq.com>
2018-12-24 10:07:45 +0800

    修改全局 scss 引入方式

--- vue.config.js
+++ vue.config.js
@@ -27,12 +27,14 @@ module.exports = {
         return options
       })
   },
-  pluginOptions: {
-    'sass-resources-loader': {
-      preProcessor: 'scss',
-      patterns: [
-        resolve('src/assets/styles/theme.scss')
-      ]
+  css: {
+    loaderOptions: {
+      // 给 sass-loader 传递选项
+      sass: {
+        // @/ 是 src/ 的别名
+        // 所以这里假设你有 `src/variables.scss` 这个文件
+        data: `@import "@/assets/styles/theme.scss";`
+      }
     }
   }
 }
